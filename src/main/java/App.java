9711589d944f8be061576f9356b4a14ff68f6551
Megan Warnock <megandwarnock@gmail.com>
2017-03-24 11:34:27 -0700

    add first two routes, newTeam-form, and start of interface logic

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+    Team tot = new Team("Team TOT!");
+    Team java = new Team("Java no bother");
+    Team hack = new Team("My Hack is better than yours");
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("teams", Team.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/teams/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/newTeam-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+}
