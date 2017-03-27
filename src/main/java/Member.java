110145bec8d8ab54cdd989490dca3d9eb5b3efc7
Megan Warnock and Xi Xia <megan@>
2017-03-27 11:20:33 -0700

    connected database to Member class

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,33 +1,58 @@
 import java.util.ArrayList;
 import java.util.List;
+import org.sql2o.*;
+
 
 public class Member {
-  private String mName;
-  private static List<Member> instances = new ArrayList<Member>();
-  private int mId;
+  private String name;
+  private int id;
 
   public Member(String name) {
-    mName = name;
-    instances.add(this);
-    mId = instances.size();
+    this.name = name;
   }
 
   public String getName(){
-    return mName;
+    return name;
   }
 
   public static List<Member> all() {
-    return instances;
-  }
+    String sql = "SELECT id, name FROM members";
+    try (Connection con = DB.sql2o.open()){
+      return con.createQuery(sql).executeAndFetch(Member.class);
+    }
 
-  public static void clear(){
-    instances.clear();
+  }
+  @Override
+  public boolean equals(Object otherMember) {
+    if (!(otherMember instanceof Member)) {
+      return false;
+    } else {
+      Member newMember = (Member) otherMember;
+      return this.getName().equals(newMember.getName()) && this.getId() == newMember.getId();
+    }
   }
   public int getId(){
-    return mId;
+    return id;
   }
 
   public static Member find(int id){
-    return instances.get(id - 1);
+    try(Connection con = DB.sql2o.open()){
+      String sql = "SELECT * FROM members where id=:id";
+      Member member = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Member.class);
+        return member;
+    }
+
+  }
+
+  public void save (){
+    try(Connection con = DB.sql2o.open()){
+      String sql = "INSERT INTO members (name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name) //"name" needs to match (:name) above.
+      .executeUpdate()
+      .getKey();
+    }
   }
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -1,7 +1,20 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
+
 
 public class MemberTest {
+  @Before
+  public void setUp(){
+    DB.sql2o = new Sql2o ("jdbc:postgresql://localhost:5432/team_tracker_test", null, null);
+  }
+  @After
+  public void tearDown(){
+    try (Connection con = DB.sql2o.open()){
+      String sql = "DELETE FROM members *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
 
   @Test
   public void Member_instantiatesCorrectly_true() {
@@ -16,31 +29,57 @@ public class MemberTest {
   }
 
   @Test
-  public void all_returnsAllInstancesOfMember_true(){
+  public void equals_returnsTrueIfNameAretheSame() {
     Member firstMember = new Member("name");
     Member secondMember = new Member("name");
-    assertEquals(true, Member.all().contains(firstMember));
-    assertEquals(true, Member.all().contains(secondMember));
+    assertTrue(firstMember.equals(secondMember));
+ }
+
+  
+
+  @Test
+  public void save_returnsTrueIfNameAretheSame() {
+    Member myMember = new Member ("name");
+    myMember.save();
+    assertTrue(Member.all().get(0).equals(myMember));
   }
+
+  @Test
+  public void all_returnsAllInstancesOfMember_true(){
+    Member firstMember = new Member("name");
+    firstMember.save();
+    Member secondMember = new Member("sam");
+    secondMember.save();
+    assertEquals(true, Member.all().get(0).equals(firstMember));
+    assertEquals(true, Member.all().get(1).equals(secondMember));
+  }
+  @Test
+  public void save_assignsIdToObject() {
+    Member myMember = new Member("name");
+    myMember.save();
+    Member savedMember = Member.all().get(0);
+    assertEquals(myMember.getId(), savedMember.getId());
+  }
+
   @Test
   public void clear_emptiesAllMembersFromArrayList_0(){
     Member newMember = new Member("name");
-    Member.clear();
     assertEquals(0, Member.all().size());
   }
 
   @Test
   public void getId_MemberInstantiatesWithId_1(){
-  Member.clear();
-  Member newMember = new Member("name");
-  assertEquals(1, newMember.getId());
+  Member myMember = new Member("name");
+  myMember.save();
+  assertTrue(myMember.getId() > 0);
   }
 
   @Test
   public void find_returnsMembersWithTheSameId_secondMember() {
-    Member.clear();
     Member firstMember = new Member("name");
-    Member secondMember = new Member("name");
+    firstMember.save();
+    Member secondMember = new Member("sam");
+    secondMember.save();
     assertEquals(secondMember, Member.find(secondMember.getId()));
   }
 }
